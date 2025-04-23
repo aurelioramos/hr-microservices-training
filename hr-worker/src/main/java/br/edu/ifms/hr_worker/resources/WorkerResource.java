38c6738017ce7f445c2bb1e8422fcaaff0f90a25
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-04-23 16:37:08 -0400

    Add config-server client

--- hr-worker/src/main/java/br/edu/ifms/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/edu/ifms/hr_worker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,9 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     private WorkerRepository repository;
 
     public WorkerResource(WorkerRepository repository) {
@@ -35,12 +39,18 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
-        Optional<Worker> worker = repository.findById(id);  
-        
+        Optional<Worker> worker = repository.findById(id);
+
         if (worker.isPresent()) {
             logger.info("Returning workerId={}", worker.get().getId());
             return ResponseEntity.ok(worker.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<String> getConfigs() {
+        logger.info("CONFIG={}", testConfig);
+        return ResponseEntity.ok(testConfig);
     }
 }
