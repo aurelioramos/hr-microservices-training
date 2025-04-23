6baeee565bf751cbd078158af0fc64a4b5a5a563
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-04-23 17:25:26 -0400

    Add draft of github credentials

--- hr-worker/src/main/java/br/edu/ifms/hr_worker/HrWorkerApplication.java
+++ hr-worker/src/main/java/br/edu/ifms/hr_worker/HrWorkerApplication.java
@@ -3,9 +3,7 @@ package br.edu.ifms.hr_worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 public class HrWorkerApplication {
