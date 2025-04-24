182c15c373157991749c68af3b885a4a202f6d76
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-04-24 17:09:56 -0400

    Project hr-authorization-server created

---
+++ hr-authorization-server/src/main/java/br/edu/ifms/hr_authorization_server/HrAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.hr_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ hr-authorization-server/src/test/java/br/edu/ifms/hr_authorization_server/HrAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.hr_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
