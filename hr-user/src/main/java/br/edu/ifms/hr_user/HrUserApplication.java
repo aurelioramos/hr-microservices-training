add208e114e599dce89d01d8578710eb7f98cf3c
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-04-24 16:20:19 -0400

    Add BCryptPasswordEncoder sample

--- hr-user/src/main/java/br/edu/ifms/hr_user/HrUserApplication.java
+++ hr-user/src/main/java/br/edu/ifms/hr_user/HrUserApplication.java
@@ -1,15 +1,30 @@
 package br.edu.ifms.hr_user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("BCRYPT = {}", passwordEncoder.encode("123"));
 	}
 
 }
