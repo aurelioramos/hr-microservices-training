1b8dba06c67567041fc96bbf1d42d613c7251977
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-04-24 16:42:31 -0400

    Add user resource

---
+++ hr-user/src/main/java/br/edu/ifms/hr_user/resources/UserResource.java
@@ -0,0 +1,36 @@
+package br.edu.ifms.hr_user.resources;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifms.hr_user.entities.User;
+import br.edu.ifms.hr_user.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
+
+    private UserRepository repository;
+
+    public UserResource(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        Optional<User> result = repository.findById(id);
+        if (result.isPresent()) {
+            return ResponseEntity.ok(result.get());
+        }
+        return ResponseEntity.status(HttpStatusCode.valueOf(204)).build();
+    }
+}
