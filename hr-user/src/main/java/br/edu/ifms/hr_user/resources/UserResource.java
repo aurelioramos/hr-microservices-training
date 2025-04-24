742a719f4ba54c2007297d27850a11cf2ad40f4a
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-04-24 16:58:21 -0400

    Add endpoint to search by email in user resource

--- hr-user/src/main/java/br/edu/ifms/hr_user/resources/UserResource.java
+++ hr-user/src/main/java/br/edu/ifms/hr_user/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifms.hr_user.entities.User;
@@ -32,5 +33,11 @@ public class UserResource {
             return ResponseEntity.ok(result.get());
         }
         return ResponseEntity.status(HttpStatusCode.valueOf(204)).build();
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> search(@RequestParam String email) {
+        User result = repository.findByEmail(email);
+        return ResponseEntity.ok(result);
     }
 }
