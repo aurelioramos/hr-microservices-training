6ce234493e87848fec86ec7784ad537c76b2188e
Aurelio Ramos <aurelio.ramos@ifms.edu.br>
2025-04-24 23:00:14 -0400

    Enable service discovery of hr-authorization-server

--- hr-authorization-server/src/main/java/br/edu/ifms/hr_authorization_server/HrAuthorizationServerApplication.java
+++ hr-authorization-server/src/main/java/br/edu/ifms/hr_authorization_server/HrAuthorizationServerApplication.java
@@ -2,7 +2,9 @@ package br.edu.ifms.hr_authorization_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HrAuthorizationServerApplication {
 
